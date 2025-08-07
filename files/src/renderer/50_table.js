eb448acaceed39f4e3e9be70f9baba76f73aba40
Yongyi Chen <yongyi781@outlook.com>
2025-08-07 10:20:19 -0400

    Convert stockfish tablebase evals to human readable format

--- files/src/renderer/50_table.js
+++ files/src/renderer/50_table.js
@@ -9,32 +9,36 @@ function NewTable() {
 }
 
 const table_prototype = {
-
-	clear: function() {
-		this.moveinfo = Object.create(null);	// move --> info
-		this.version = 0;						// Incremented on any change
-		this.nodes = 0;							// Stat sent by engine
-		this.nps = 0;							// Stat sent by engine
+	clear: function () {
+		this.moveinfo = Object.create(null); // move --> info
+		this.version = 0; // Incremented on any change
+		this.nodes = 0; // Stat sent by engine
+		this.nps = 0; // Stat sent by engine
 		this.hashfull = 0;
-		this.tbhits = 0;						// Stat sent by engine
-		this.time = 0;							// Stat sent by engine
-		this.limit = null;						// The limit of the last search that updated this.
-		this.terminal = null;					// null = unknown, "" = not terminal, "Non-empty string" = terminal reason
-		this.graph_y = null;					// Used by grapher only, value from White's POV between 0 and 1
-		this.graph_y_version = 0;				// Which version (above) was used to generate the graph_y value
+		this.tbhits = 0; // Stat sent by engine
+		this.time = 0; // Stat sent by engine
+		this.limit = null; // The limit of the last search that updated this.
+		this.terminal = null; // null = unknown, "" = not terminal, "Non-empty string" = terminal reason
+		this.graph_y = null; // Used by grapher only, value from White's POV between 0 and 1
+		this.graph_y_version = 0; // Which version (above) was used to generate the graph_y value
 		this.already_autopopulated = false;
 	},
 
-	get_graph_y: function() {
-
+	get_graph_y: function () {
 		// Naphthalin's scheme: based on centipawns.
 
 		if (this.graph_y_version === this.version) {
 			return this.graph_y;
 		} else {
 			let info = SortedMoveInfoFromTable(this)[0];
-			if (info && !info.__ghost && info.__touched && (this.nodes > 1 || this.limit === 1)) {
-				this.graph_y = info.board.active === "w" ? info.cp / 100 : -info.cp / 100;
+			if (
+				info &&
+				!info.__ghost &&
+				info.__touched &&
+				(this.nodes > 1 || this.limit === 1)
+			) {
+				this.graph_y =
+					info.board.active === "w" ? info.cp / 100 : -info.cp / 100;
 				// let cp = info.cp;
 				// if (info.board.active === "b") {
 				// 	cp *= -1;
@@ -48,7 +52,8 @@ const table_prototype = {
 		}
 	},
 
-	set_terminal_info: function(reason, ev) {	// ev is ignored if reason is "" (i.e. not a terminal position)
+	set_terminal_info: function (reason, ev) {
+		// ev is ignored if reason is "" (i.e. not a terminal position)
 		if (reason) {
 			this.terminal = reason;
 			this.graph_y = ev;
@@ -58,8 +63,7 @@ const table_prototype = {
 		}
 	},
 
-	autopopulate: function(node) {
-
+	autopopulate: function (node) {
 		if (!node) {
 			throw "autopopulate() requires node argument";
 		}
@@ -81,7 +85,7 @@ const table_prototype = {
 		}
 
 		this.already_autopopulated = true;
-	}
+	},
 };
 
 // --------------------------------------------------------------------------------------------
@@ -89,17 +93,16 @@ const table_prototype = {
 // the object takes place in info.js and the ih.receive() method there.
 
 function NewInfo(board, move) {
-
 	let info = Object.create(info_prototype);
 
 	info.board = board;
 	info.move = move;
-	info.__ghost = false;			// If not false, this is temporary inferred info.
-	info.__touched = false;			// Has this ever actually been updated?
-	info.leelaish = false;			// Whether the most recent update to this info was from an engine considered Leelaish.
-	info.pv = [move];				// Validated as a legal sequence upon reception.
-	info.cycle = 0;					// How many "go" commands Nibbler has emitted.
-	info.subcycle = 0;				// How many "blocks" of info we have seen (delineated by multipv 1 info).
+	info.__ghost = false; // If not false, this is temporary inferred info.
+	info.__touched = false; // Has this ever actually been updated?
+	info.leelaish = false; // Whether the most recent update to this info was from an engine considered Leelaish.
+	info.pv = [move]; // Validated as a legal sequence upon reception.
+	info.cycle = 0; // How many "go" commands Nibbler has emitted.
+	info.subcycle = 0; // How many "blocks" of info we have seen (delineated by multipv 1 info).
 
 	info.nice_pv_cache = [board.nice_string(move)];
 
@@ -108,37 +111,35 @@ function NewInfo(board, move) {
 }
 
 const info_prototype = {
-
 	// I'm not sure I've been conscientious everywhere in the code about checking whether these things are
 	// of the right type, so for that reason most are set to some neutralish value by default.
 	//
 	// Exceptions: m, v, wdl (and note that all of these can be set to null by info.js)
 
-	clear_stats: function() {
+	clear_stats: function () {
 		this.cp = 0;
 		this.depth = 0;
 		this.m = null;
-		this.mate = 0;				// 0 can be the "not present" value.
+		this.mate = 0; // 0 can be the "not present" value.
 		this.multipv = 1;
 		this.n = 0;
-		this.p = 0;					// Note P is received and stored as a percent, e.g. 31.76 is a reasonable P.
+		this.p = 0; // Note P is received and stored as a percent, e.g. 31.76 is a reasonable P.
 		this.q = 0;
-		this.s = 1;					// Known as Q+U before Lc0 v0.25-rc2
+		this.s = 1; // Known as Q+U before Lc0 v0.25-rc2
 		this.seldepth = 0;
 		this.u = 1;
-		this.uci_nodes = 0;			// The number of nodes reported by the UCI info lines (i.e. for the whole position).
+		this.uci_nodes = 0; // The number of nodes reported by the UCI info lines (i.e. for the whole position).
 		this.v = null;
-		this.vms_order = 0;			// VerboseMoveStats order, 0 means not present, 1 is the worst, higher is better.
-		this.wdl = null;			// Either null or a length 3 array of ints.
+		this.vms_order = 0; // VerboseMoveStats order, 0 means not present, 1 is the worst, higher is better.
+		this.wdl = null; // Either null or a length 3 array of ints.
 	},
 
-	set_pv: function(pv) {
+	set_pv: function (pv) {
 		this.pv = Array.from(pv);
 		this.nice_pv_cache = null;
 	},
 
-	nice_pv: function() {
-
+	nice_pv: function () {
 		// Human readable moves.
 
 		if (this.nice_pv_cache) {
@@ -147,16 +148,16 @@ const info_prototype = {
 
 		let tmp_board = this.board;
 
-		if (!this.pv || this.pv.length === 0) {			// Should be impossible.
+		if (!this.pv || this.pv.length === 0) {
+			// Should be impossible.
 			this.pv = [this.move];
 		}
 
 		let ret = [];
 
 		for (let move of this.pv) {
-
 			// if (tmp_board.illegal(move)) break;		// Should be impossible as of 1.8.4: PVs are validated upon reception, and the only other
-														// way they can get changed is by maybe_infer_info(), which hopefully is sound.
+			// way they can get changed is by maybe_infer_info(), which hopefully is sound.
 			ret.push(tmp_board.nice_string(move));
 			tmp_board = tmp_board.move(move);
 		}
@@ -165,11 +166,11 @@ const info_prototype = {
 		return Array.from(this.nice_pv_cache);
 	},
 
-	value: function() {
-		return Value(this.q);		// Rescaled to 0..1
+	value: function () {
+		return Value(this.q); // Rescaled to 0..1
 	},
 
-	value_string: function(dp, pov) {
+	value_string: function (dp, pov) {
 		if (!this.__touched || typeof this.q !== "number") {
 			return "?";
 		}
@@ -179,11 +180,11 @@ const info_prototype = {
 		if (typeof this.mate === "number" && this.mate !== 0) {
 			return this.mate_string(pov);
 		}
-	 	return this.cp_string(pov, dp);
+		return this.cp_string(pov, dp);
 	},
 
 	// Returns pawns
-	cp_with_pov: function(pov) {
+	cp_with_pov: function (pov) {
 		if (!this.__touched || typeof this.cp !== "number") {
 			return NaN;
 		}
@@ -191,16 +192,23 @@ const info_prototype = {
 			return NaN;
 		}
 		let cp = this.cp;
-		if ((pov === "w" && this.board.active === "b") || (pov === "b" && this.board.active === "w")) {
+		if (
+			(pov === "w" && this.board.active === "b") ||
+			(pov === "b" && this.board.active === "w")
+		) {
 			cp = -cp;
 		}
 		return cp / 100;
 	},
 
-	cp_string: function(pov, dp=2) {
+	cp_string: function (pov, dp = 2) {
 		let cp = this.cp_with_pov(pov);
 		if (isNaN(cp)) {
 			return "?";
+		}
+		if (Math.abs(cp) >= 150) {
+			const plies = Math.round((200 - Math.abs(cp)) * 100);
+			return `${cp > 0 ? "+" : "-"}T${plies}`;
 		}
 		let ret = cp.toFixed(dp);
 		if (cp > 0) {
@@ -209,12 +217,15 @@ const info_prototype = {
 		return ret;
 	},
 
-	mate_string: function(pov) {
+	mate_string: function (pov) {
 		if (typeof this.mate !== "number" || this.mate === 0) {
 			return "?";
 		}
 		let mate = this.mate;
-		if ((pov === "w" && this.board.active === "b") || (pov === "b" && this.board.active === "w")) {
+		if (
+			(pov === "w" && this.board.active === "b") ||
+			(pov === "b" && this.board.active === "w")
+		) {
 			mate = 0 - mate;
 		}
 		if (mate < 0) {
@@ -224,18 +235,22 @@ const info_prototype = {
 		}
 	},
 
-	wdl_string: function(pov) {
+	wdl_string: function (pov) {
 		if (Array.isArray(this.wdl) === false || this.wdl.length !== 3) {
 			return "?";
 		}
-		if ((pov === "w" && this.board.active === "b") || (pov === "b" && this.board.active === "w")) {
+		if (
+			(pov === "w" && this.board.active === "b") ||
+			(pov === "b" && this.board.active === "w")
+		) {
 			return `${this.wdl[2]} ${this.wdl[1]} ${this.wdl[0]}`;
 		} else {
 			return `${this.wdl[0]} ${this.wdl[1]} ${this.wdl[2]}`;
 		}
 	},
 
-	stats_list: function(opts, total_nodes) {		// We pass total_nodes rather than use this.uci_nodes which can be obsolete (e.g. due to searchmoves)
+	stats_list: function (opts, total_nodes) {
+		// We pass total_nodes rather than use this.uci_nodes which can be obsolete (e.g. due to searchmoves)
 
 		if (this.__ghost) {
 			return ["Inferred"];
@@ -254,13 +269,15 @@ const info_prototype = {
 		// N is fairly complicated...
 
 		if (this.leelaish) {
-
-			if (typeof this.n === "number" && total_nodes) {		// i.e. total_nodes is not zero or undefined
+			if (typeof this.n === "number" && total_nodes) {
+				// i.e. total_nodes is not zero or undefined
 
 				let n_string = "";
 
 				if (opts.n) {
-					n_string += ` N: ${(100 * this.n / total_nodes).toFixed(2)}%`;
+					n_string += ` N: ${((100 * this.n) / total_nodes).toFixed(
+						2
+					)}%`;
 				}
 
 				if (opts.n_abs) {
@@ -278,13 +295,10 @@ const info_prototype = {
 				if (n_string !== "") {
 					ret.push(n_string.trim());
 				}
-
 			} else {
-
 				if (opts.n || opts.n_abs || opts.of_n) {
 					ret.push("N: ?");
 				}
-
 			}
 		}
 
@@ -327,14 +341,16 @@ const info_prototype = {
 
 		if (this.leelaish) {
 			if (opts.u) {
-				if (typeof this.u === "number" && this.n > 0) {						// Checking n is correct.
+				if (typeof this.u === "number" && this.n > 0) {
+					// Checking n is correct.
 					ret.push(`U: ${this.u.toFixed(3)}`);
 				} else {
 					ret.push(`U: ?`);
 				}
 			}
 			if (opts.s) {
-				if (typeof this.s === "number" && this.n > 0) {						// Checking n is correct.
+				if (typeof this.s === "number" && this.n > 0) {
+					// Checking n is correct.
 					ret.push(`S: ${this.s.toFixed(5)}`);
 				} else {
 					ret.push(`S: ?`);
@@ -358,5 +374,5 @@ const info_prototype = {
 		}
 
 		return ret;
-	}
+	},
 };
